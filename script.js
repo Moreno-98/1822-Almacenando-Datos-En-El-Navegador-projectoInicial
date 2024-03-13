17d97297d69df798a4a433ef4750cf1d29ea4c96
christian <christian@gmail.com>
2024-03-13 11:52:29 -0500

    Edición de listas

--- script.js
+++ script.js
@@ -3,28 +3,56 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+//creando funcion//
+const addTask = (evento) =>{
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  
+// Agregar nuevos elementos a un documento existente o para mover un elemento de la página//  
+  list.appendChild(task);
+}
+
+
+//creando funcion//
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
+  const date = calendar.value;
+  //cambiando formato//
+  const dateformat = moment(date).format('DD/MM/YYYY');
   const value = input.value;
-  const list = document.querySelector('[data-list]');
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
   //backticks
+
   const taskContent = document.createElement('div');
 
+  //almacenamiento en el navegador local y sesesionstorage
+  console.log(value, dateformat);
+  const taskobj = {
+    value,
+    dateformat
+  }
+  localStorage.setItem('task', JSON.stringify(taskobj))
+
+  //creando la tarjeta//
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
+  
+  const dateElement = document.createElement('span')
+  dateElement.innerHTML = dateformat;
 
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
